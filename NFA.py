6ee23305d24066f26ab7974d7a129b181a92301b
Alex Jose <ajose@caltech.edu>
2014-06-18 12:43:20 -0400

    seems to work

--- NFA.py
+++ NFA.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+import matplotlib.pyplot as plt
 import networkx as nx
+from metachars import Metachars as M
 import uuid
 
 """
@@ -16,88 +18,111 @@ TODO:
 - Could also attempt to make a NFA to DFA converter here
 - Also maybe something to trim useless states like from that one article
 - Add a function like "unmark_accept_states"...could include an exception list
+- add a copy function that makes new UIDs but preserves data
+- lots of repeated epsilon edges - problem? could just trim...
 """
 
-def uid(self, ):
-    return uuid.uuid4()
+def uid():
+    return str(uuid.uuid4())
 
 # actually usually partial NFAs...
 class NFA:
     # could make a function map dictionary
     
-    def __init__(self, symbol, nfa_list*):
+    def __init__(self, symbol, *nfa_list):
         self.G = nx.DiGraph()
+        # ehhh, hack
+        self.func_mapping = {'|':self.make_union,
+                             '&':self.make_concatenation,
+                             '*':self.make_kleene,
+                             '+':self.make_plus,
+                             '?':self.make_question,
+                             }
+        # operand
+        if symbol not in M.metachars.keys():
+            self.make_character(symbol, nfa_list)
+        elif symbol in self.func_mapping.keys():
+            self.func_mapping[symbol](symbol, nfa_list)
+        # SHOULD MAKE SYMBOSL AND STUFF FROM regex file available everywhere...
+        # just put in a third file and import it I guess
     
-    def get_start_states(self, nfa, num_expected=None):
+    def get_start_states(self, num_expected=None):
         # probably better way to do this...
-        nodes = [g for g,d in nfa.nodes(data=True) if d['start']]
+        #print self.G.nodes(data=True)
+        nodes = [g for g,d in self.G.nodes(data=True) if d['start']]
         if num_expected: assert(len(nodes) == num_expected)
         return nodes
 
-    def get_accept_states(self, nfa, num_expected=None):
+    def get_accept_states(self, num_expected=None):
         # need num_expected?
-        nodes = [g for g,d in nfa.nodes(data=True) if d['accept']]
+        nodes = [g for g,d in self.G.nodes(data=True) if d['accept']]
         if num_expected: assert(len(nodes) == num_expected)
         return nodes
 
-    def make_character(self, char):
+    def make_character(self, symbol, nfa_list):
         n1, n2 = uid(), uid()
         self.G.add_node(n1, start=True, accept=False)
         self.G.add_node(n2, start=False, accept=True)
-        self.G.add_edge(n1, n2, label=char)
+        self.G.add_edge(n1, n2, label=symbol)
 
-    def make_union(self, nfa_list):
+    def make_union(self, symbol, nfa_list):
         assert(len(nfa_list) == 2)
         nfa1, nfa2 = nfa_list
         # copy other NFAs into own graph
-        self.G.add_nodes_from(nfa1)
-        self.G.add_nodes_from(nfa2)
+        self.G.add_nodes_from(nfa1.G.nodes(data=True))
+        self.G.add_nodes_from(nfa2.G.nodes(data=True))
+        self.G.add_edges_from(nfa1.G.edges(data=True))
+        self.G.add_edges_from(nfa2.G.edges(data=True))
         # find start and accept states
-        start_states = get_start_states(self.G)
-        accept_states = get_accept_states(self.G)
+        start = self.get_start_states()
+        accept = self.get_accept_states()
         # add new start state and update old ones
         new_start = uid()
         self.G.add_node(new_start, start=True, accept=False)
-        for start in start_states:
-            self.G.add_edge(new_start, start, label=None)
-            self.G.node[start]['start'] = False
+        for n in start:
+            self.G.add_edge(new_start, n, label=None)
+            self.G.node[n]['start'] = False
         # add new accept state and update old ones
         new_accept = uid()
         self.G.add_node(new_accept, start=False, accept=True)
-        for accept in accept_states:
-            self.G.add_edge(new_accept, accept, label=None)
-            self.G.node[start]['accept'] = False
+        for n in accept:
+            self.G.add_edge(n, new_accept, label=None)
+            self.G.node[n]['accept'] = False
 
-    def make_concatenation(self, nfa_list):
+    def make_concatenation(self, symbol, nfa_list):
         assert(len(nfa_list) == 2)
+        # assume first NFA in list comes first in concatenation
         nfa1, nfa2 = nfa_list
         # new start and accept states
         new_start, new_accept = uid(), uid()
         self.G.add_node(new_start, start=True, accept=False)
         self.G.add_node(new_accept, start=False, accept=True)
-        # assume first NFA in list comes first in concatenation
-        self.G.add_nodes_from(nfa_list[0])
         # get start and accept states
-        start1 = self.get_start_states(nfa1)
-        start2 = self.get_start_states(nfa2)
-        accept1 = self.get_accept_states(nfa1)
-        accept2 = self.get_accept_states(nfa2)
+        start1 = nfa1.get_start_states()
+        start2 = nfa2.get_start_states()
+        accept1 = nfa1.get_accept_states()
+        accept2 = nfa2.get_accept_states()
         # connect things
-        self.G.add_nodes_from(nfa1)
-        self.G.add_nodes_from(nfa2)
+        self.G.add_nodes_from(nfa1.G.nodes(data=True))
+        self.G.add_nodes_from(nfa2.G.nodes(data=True))
+        self.G.add_edges_from(nfa1.G.edges(data=True))
+        self.G.add_edges_from(nfa2.G.edges(data=True))
+        # connect new start to nfa1's starts
         for n in start1:
             self.G.add_edge(new_start, n, label=None)
             self.G.node[n]['start'] = False
+        # connect nfa2's accepts to new accepts
         for n in accept2:
             self.G.add_edge(n, new_accept, label=None)
             self.G.node[n]['accept'] = False
+        # connect nfa1's accepts to nfa2's starts
         for n1 in accept1:
             for n2 in start2:
                 self.G.add_edge(n1, n2, label=None)
-                self.G.node[n1]['accept'] = "False"
-                self.G.node[n2]['start'] = "False"
-        
-    def make_kleene(self, nfa_list):
+                self.G.node[n1]['accept'] = False
+                self.G.node[n2]['start'] = False
+
+    def make_kleene(self, symbol, nfa_list):
         assert(len(nfa_list) == 1)
         nfa = nfa_list[0]
         # new start and accept states
@@ -107,32 +132,35 @@ class NFA:
         # also add an edge directly to accept
         self.G.add_edge(new_start, new_accept, label=None)
         # get start, accept states and add nodes from nfa
-        start = self.get_start_states(nfa)
-        accept = self.get_accept_states(nfa)
-        self.G.add_nodes_from(nfa)
+        start = nfa.get_start_states()
+        accept = nfa.get_accept_states()
+        self.G.add_nodes_from(nfa.G.nodes(data=True))
+        self.G.add_edges_from(nfa.G.edges(data=True))
         # connect and update nodes
         for n in start:
-            self.G.add_edge(new_start, start, label=None)
-            self.G.node[start]['start'] = False
+            self.G.add_edge(new_start, n, label=None)
+            self.G.node[n]['start'] = False
         for n in accept:
             # connect old NFA's accept states to new start
-            self.G.add_edge(accept new_start, label=None)
-            self.G.node[start]['accept'] = False
+            self.G.add_edge(n, new_start, label=None)
+            self.G.node[n]['accept'] = False
 
-    def make_plus(self, nfa_list):
+    def make_plus(self, symbol, nfa_list):
         assert(len(nfa_list) == 1)
         nfa = nfa_list[0]
-        # new start and accept states, with addition of an intermediate state
-        new_start, new_accept, intermediate = uid(), uid(), uid()
-        self.G.add_node(new_start, start=True, accept=False)
+        # new start and accpt states, with addition of an intermediate state
+        #new_start, new_accept, intermediate = uid(), uid(), uid()
+        new_accept, intermediate = uid(), uid()
+        #self.G.add_node(new_start, start=True, accept=False)
         self.G.add_node(new_accept, start=False, accept=True)
         self.G.add_node(intermediate, start=False, accept=False)
         # add transition to accept state from intermediate
         self.G.add_edge(intermediate, new_accept, label=None)
         # get accept states and add nodes from nfa
-        start = self.get_start_states(nfa)
-        accept = self.get_accept_states(nfa)
-        self.G.add_nodes_from(nfa)
+        start = nfa.get_start_states()
+        accept = nfa.get_accept_states()
+        self.G.add_nodes_from(nfa.G.nodes(data=True))
+        self.G.add_edges_from(nfa.G.edges(data=True))
         # connect and update nodes
         for n in accept:
             self.G.add_edge(n, intermediate, label=None)
@@ -141,7 +169,7 @@ class NFA:
             # start states actually stay the same
             self.G.add_edge(intermediate, n, label=None)
 
-    def make_question(self, nfa_list):
+    def make_question(self, symbol, nfa_list):
         assert(len(nfa_list) == 1)
         nfa = nfa_list[0]
         # new start and accept states
@@ -151,9 +179,10 @@ class NFA:
         # also add an edge directly to accept
         self.G.add_edge(new_start, new_accept, label=None)
         # get start, accept states and add nodes from nfa
-        start = self.get_start_states(nfa)
-        accept = self.get_accept_states(nfa)
-        self.G.add_nodes_from(nfa)
+        start = nfa.get_start_states()
+        accept = nfa.get_accept_states()
+        self.G.add_nodes_from(nfa.G.nodes(data=True))
+        self.G.add_edges_from(nfa.G.edges(data=True))
         # connect and update nodes
         for n in start:
             self.G.add_edge(new_start, n, label=None)
@@ -162,7 +191,39 @@ class NFA:
             self.G.add_edge(n, new_accept, label=None)
             self.G.node[n]['accept'] = False
         
-
+    def display(self):
+        # should hide names but show start and accept nodes differently
+        # also show edge labels
+        ncolors = []
+        for n in self.G:
+            if self.G.node[n]['start']:
+                ncolors.append('green')
+            elif self.G.node[n]['accept']:
+                ncolors.append('red')
+            else:
+                ncolors.append('blue')
+        elabels = dict()
+        for p,s in self.G.edges():
+            lbl = self.G.edge[p][s]['label']
+            if lbl != None: elabels[(p,s)] = r'$'+lbl+'$'
+            else: elabels[(p,s)] = r'$\epsilon$'
+        layout = nx.graphviz_layout(self.G)
+        nx.draw_networkx(self.G, with_labels=False, pos=layout,
+                         node_color=ncolors)
+        nx.draw_networkx_edge_labels(self.G, pos=layout, edge_labels=elabels,
+                                     font_size=20)
+        #plt.draw()
+        plt.show()
 
 if __name__=='__main__':
-    pass
+    a = NFA('a')
+    b = NFA('b')
+    c = NFA('c')
+    c_kleene = NFA('*', c)
+    c_plus = NFA('+', c)
+    c_question = NFA('?', c)
+    a_or_b = NFA('|', a, b)
+    a_or_b_and_c = NFA('&', a_or_b, c)
+    a_or_b_and_c_kleene = NFA('*', a_or_b_and_c)
+    a_or_b_and_c_plus = NFA('+', a_or_b_and_c)
+    a_or_b_and_c_plus.display()
